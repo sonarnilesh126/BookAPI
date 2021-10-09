6ac1256fe7e9a4986b0f07399da63fcd96eaee30
sonarnilesh126 <nileshsonar126@gmail.com>
2021-10-09 13:33:33 +0530

    add BOOKB2I

---
+++ requirements.js
@@ -0,0 +1,36 @@
+// Requirements for our project
+
+// We are the bool management company
+
+// Books
+// ISBN, title, pub date, langauge, no of page, author[], category []
+
+// Authors
+// id, name, books[]
+
+// Publications
+// id, name, books[]
+
+// what are all the APIs that we need...
+
+// Books
+// We need an API
+// to get all books 
+// to get specific books
+// to get list of books based on category
+// to get list of books based on languages
+
+// Authors
+
+// We need an api
+// to get all authors 
+// to get specific authors
+// to get list of authors based on books
+
+// Publication
+
+// we need an API
+// to get all publication
+// to get specific publication
+// to get list of publication based on books
+
